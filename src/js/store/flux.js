75fa091f07762f7325311ecb45ccea6fc8e186ce
Anahi Vera <168015268+AnahiVera@users.noreply.github.com>
2024-09-09 20:28:48 +0000

    finalizado T-T

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,110 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+    return {
+        store: {
+            baseUrl: "https://playground.4geeks.com",
+            contactsList: [],
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+
+        },
+        actions: {
+
+            getContactList: () => {
+                const { baseUrl } = getStore()
+                fetch(`${baseUrl}/contact/agendas/AnahiVera`)
+                    .then((response) => {
+                        if (!response.ok) {
+                            throw new Error("Network response was not ok");
+                        }
+                        return response.json();
+                    })
+                    .then((responseJson) => setStore({ contactsList: responseJson.contacts })) /* cambiarsetTodos */
+                    .catch((error) => console.log("Error fetching:", error));
+            },
+            addContact: (newContact) => {
+                const options = {
+                    method: 'POST',
+                    body: JSON.stringify(newContact),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+                const { baseUrl, contactsList } = getStore()
+                fetch(`${baseUrl}/contact/agendas/AnahiVera/contacts`, options)
+                    .then((response) => {
+                        if (!response.ok) {
+                            throw new Error("Failed to save");
+                        }
+                        return response.json();
+                    })
+                    .then((responseJson) => setStore({ contactsList: [...contactsList, responseJson] }))
+                    .catch((error) => console.log(" Error saving", error))
+            },
+            updateContact: (contact, id) => {
+                const options = {
+                    method: 'PUT',
+                    body: JSON.stringify(contact),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+                const { baseUrl, contactsList } = getStore()
+                fetch(`${baseUrl}/contact/agendas/AnahiVera/contacts/${id}`, options)
+                    .then((response) => {
+                        if (!response.ok) {
+                            throw new Error("Failed to update");
+                        }
+                        return response.json();
+                    })
+                    .then((responseJson) => {
+                        getActions().getContactList()
+                    })
+                    .catch((error) => console.log(" Error updating contact", error))
+            },
+            deleteContact: (id) => {
+                const options = {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+                const { baseUrl, contactsList } = getStore()
+                fetch(`${baseUrl}/contact/agendas/AnahiVera/contacts/${id}`, options)
+                    .then((response) => {
+                        if (!response.ok) {
+                            throw new Error("Failed to delete")
+                        } else {
+                            setStore({ contactsList: contactsList.filter((contact) => contact.id !== id) });
+                        }
+
+                    })
+                    .catch((error) => console.log("Error deleting todo:", error))
+            },
+            handleSubmit: (e) => {
+                e.preventDefault();
+                const newContact = {
+                    name: e.target.name.value,
+                    phone: e.target.phone.value,
+                    email: e.target.email.value,
+                    address: e.target.address.value
+                }
+                const actions = getActions()
+                actions.addContact(newContact);
+            },
+            handleUpdate: (e, id) => {
+                e.preventDefault();
+                const updateContact = {
+                    name: e.target.name.value,
+                    phone: e.target.phone.value,
+                    email: e.target.email.value,
+                    address: e.target.address.value
+                }
+                const actions = getActions()
+                actions.updateContact(updateContact, id);
+            },
+
+        }
+    };
 };
 
 export default getState;
